5ca6fad5e98516bf4a7cb36314735159ece143db
NARENTHIRANATH AS <narenthiranath703@gmail.com>
2026-06-30 13:43:34 +0530

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -10,12 +10,7 @@ model = joblib.load(BASE_DIR / "model" / "house_price_model.pkl")
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:8080",
-    "http://localhost:8080",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"
-    ],
+   allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
